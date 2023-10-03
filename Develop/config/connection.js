c3a2425766a38e591f0d8c844ffc2fe36cc31ec3
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-10-03 17:39:21 -0600

    resolving ECONNREFUSED so .env works as intended

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,11 +4,11 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
 ? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize("ecommerce_db", "root", null, {
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     // const sequelize = new sequelize.Sequelize("ecommerce_db", "root", null{
     //   host: "localhost",
     // })
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
