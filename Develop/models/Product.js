1845b4e8f316b79e123503aee82c38a9cc0fbdfd
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-10-03 00:24:51 -0600

    resolve syntax errors

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -12,7 +12,7 @@ Product.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false;
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
@@ -30,7 +30,7 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      validate {isNumeric: true}
+      validate: {isNumeric: true}
     },
     category_id: {
       type: DataTypes.INTEGER,
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -162,5 +162,5 @@ router.delete('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-
+  });
 module.exports = router;
